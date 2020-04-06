4369ec603edb4d609439e494a43650dccec5130a
vpavliuk <vpavliuk@varteq.com>
2020-04-06 23:58:35 +0300

    set up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ParentDetailComponent} from './parent-detail/parent-detail.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { HeaderComponent } from './header/header.component';
+import { LearnDayComponent } from './schedule/learn-day/learn-day.component';
+import { GradebookComponent } from './gradebook/gradebook.component';
+import { ScoreComponent } from './gradebook/score/score.component';
+import { ParentSearchComponent } from './parent-search/parent-search.component';
+import {DatePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentDetailComponent,
+    ScheduleComponent,
+    HeaderComponent,
+    LearnDayComponent,
+    GradebookComponent,
+    ScoreComponent,
+    ParentSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
